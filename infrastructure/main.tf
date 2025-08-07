0391a6f8c3186c2f7ec7a9f3e8bd306663db1910
Yoonhojoon <dbsghwns1209@gmail.com>
2025-08-07 16:11:27 +0900

    [HOTFIX] main.tf 수정정

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -867,12 +867,12 @@ resource "aws_db_instance" "main" {
 
   engine         = "postgres"
   engine_version = "15.4"
-  instance_class = "db.t3.micro"  # 프리티어
+  instance_class = "db.t3.micro" # 프리티어
 
   allocated_storage     = 20
   max_allocated_storage = 100
-  storage_type         = "gp2"
-  storage_encrypted    = true
+  storage_type          = "gp2"
+  storage_encrypted     = true
 
   db_name  = "mclass_prod"
   username = "postgres"
@@ -882,8 +882,8 @@ resource "aws_db_instance" "main" {
   db_subnet_group_name   = aws_db_subnet_group.main.name
 
   backup_retention_period = 7
-  backup_window          = "03:00-04:00"
-  maintenance_window     = "sun:04:00-sun:05:00"
+  backup_window           = "03:00-04:00"
+  maintenance_window      = "sun:04:00-sun:05:00"
 
   skip_final_snapshot = true
   deletion_protection = false
